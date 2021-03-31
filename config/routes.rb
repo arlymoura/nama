872734b633122e4c37d4134ccdec8177c05ba5a2
Arly Moura <arlymoura@gmail.com>
2021-03-30 23:16:13 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'dashboard/new_import'
+  post 'dashboard/import'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources :dashboard do
+  #   collection do
+  #      # import_new_file_path
+  #     get 'dashboard/new', to: :new_import
+
+  #     # import_file_path
+  #     post :dashboard_import
+  #   end
+  # end
 end
