e6b86ac9974d30b0f2cf6d9d10916e94692433af
Arly Moura <arlymoura@gmail.com>
2021-03-30 23:14:11 -0300

    adjust controller to user service

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,24 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
   def index
+    @imports = Import.all
+  end
+
+  def new_import
+    @import = Import.new
   end
 
   def import
+    ImportFileService.new.execute(params[:import][:import_file])
+
+    flash[:success] = '<strong>File Imported!</strong>'
+
+    redirect_to dashboard_index_path
+  rescue StandardError => e
+    flash[:error] = "There was a problem importing that file.<br>
+        <strong>#{e.message}</strong><br>"
+
+    redirect_to dashboard_new_import_path
   end
 end
